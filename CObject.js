3f0318daf803549f7be4c11de9a9ec2a3951cb8c
GUSCRAWFORD <crawford.gus@gmail.com>
2013-06-13 16:45:18 -0300

    Update CObject.js
    Started poking around and writing some comments while at work

--- CObject.js
+++ CObject.js
@@ -1,10 +1,11 @@
-
 // Require col object
 if (window && window.col) {
 
 window.col.CObject = function _CObject () {
+	// Pre-construct
 	var CObject = this;
 	
+	// Object properties / methods
 };
 
 // Require col object
